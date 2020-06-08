a4ed8f91fc54d1c724c6d3040c34527ab272ee1f
Nino van Hooff <ninovanhooff@gmail.com>
2020-06-08 11:42:58 +0200

    Refactor retrofit api creation to WeatherService

--- app/src/main/java/com/ninovanhooff/negenwnegenw/services/WeatherService.kt
+++ app/src/main/java/com/ninovanhooff/negenwnegenw/services/WeatherService.kt
@@ -1,10 +1,11 @@
 package com.ninovanhooff.negenwnegenw.services
 
-import kotlinx.coroutines.Deferred
+import com.ninovanhooff.negenwnegenw.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Headers
 
 
 interface WeatherService {
@@ -12,6 +13,37 @@ interface WeatherService {
     @GET("forecast/?q=san francisco")
     suspend fun getForecast(): Response<FiveDayForecastResponse>
 
+    companion object {
+        val INSTANCE: WeatherService by lazy {
+            createInstance()
+        }
+
+
+        private fun createInstance(): WeatherService {
+            val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor { chain ->
+                val original = chain.request()
+
+                // Request customization: add request headers
+                val requestBuilder = original.newBuilder()
+                    .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", BuildConfig.rapidapi_key)
+
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }
+
+            val okHttp = httpClient.build()
+
+            val retrofit = Retrofit.Builder()
+                .client(okHttp)
+                .baseUrl("https://community-open-weather-map.p.rapidapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return retrofit.create(WeatherService::class.java)
+        }
+    }
 }
 
 class FiveDayForecastResponse(
--- app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/HomeViewModel.kt
+++ app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/HomeViewModel.kt
@@ -3,16 +3,12 @@ package com.ninovanhooff.negenwnegenw.ui.today
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.ninovanhooff.negenwnegenw.BuildConfig
 import com.ninovanhooff.negenwnegenw.services.WeatherService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
 import retrofit2.HttpException
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class TodayViewModel : ViewModel() {
 
@@ -22,34 +18,10 @@ class TodayViewModel : ViewModel() {
     val text: LiveData<String> = _text
 
     init {
-        val httpClient = OkHttpClient.Builder()
-        httpClient.addInterceptor { chain ->
-            val original = chain.request()
-
-            // Request customization: add request headers
-            val requestBuilder = original.newBuilder()
-                .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
-                .addHeader("x-rapidapi-key", BuildConfig.rapidapi_key)
-
-            val request = requestBuilder.build()
-            chain.proceed(request)
-        }
-
-        val okHttp = httpClient.build()
-
-        val retrofit = Retrofit.Builder()
-            .client(okHttp)
-            .baseUrl("https://community-open-weather-map.p.rapidapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service: WeatherService = retrofit.create(WeatherService::class.java)
-
-
-
+        val weather = WeatherService.INSTANCE
 
         CoroutineScope(Dispatchers.IO).launch {
-            val response = service.getForecast()
+            val response = weather.getForecast()
             withContext(Dispatchers.Default) {
                 // NOT run on main thread. dispatch / post updates to update UI.
                 try {
