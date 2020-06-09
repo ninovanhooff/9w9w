ba22dcb27a226ab861df69f4fec3c0313bc68925
Nino van Hooff <ninovanhooff@gmail.com>
2020-06-10 00:00:36 +0200

    Hook up Weather animation ViewFlipper to ViewModel

--- app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/TodayFragment.kt
+++ app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/TodayFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieCompositionFactory
 import com.ninovanhooff.negenwnegenw.databinding.FragmentTodayBinding
 
 class TodayFragment : Fragment() {
@@ -24,8 +26,27 @@ class TodayFragment : Fragment() {
 
         todayViewModel.todayModel.observe(viewLifecycleOwner, Observer {
             binding.today = it
+
+            LottieCompositionFactory.fromRawRes(context, it.animationRawRes).addListener { composition ->
+                when(binding.weatherFlipper.displayedChild) {
+                    0 -> binding.weatherAnimation1.setComposition(composition)
+                    1 -> binding.weatherAnimation0.setComposition(composition)
+                    else -> throw NotImplementedError("Unexpected ViewFlipper child index $it")
+                }
+            }
+
         })
 
+        bindLottieViews(binding)
+
         return binding.root
+    }
+
+    private fun bindLottieViews(binding: FragmentTodayBinding) {
+        val listener: (composition: LottieComposition) -> Unit = {
+            binding.weatherFlipper.showNext()
+        }
+        binding.weatherAnimation0.addLottieOnCompositionLoadedListener(listener)
+        binding.weatherAnimation1.addLottieOnCompositionLoadedListener(listener)
     }
 }
--- app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/TodayModel.kt
+++ app/src/main/java/com/ninovanhooff/negenwnegenw/ui/today/TodayModel.kt
@@ -1,5 +1,7 @@
 package com.ninovanhooff.negenwnegenw.ui.today
 
+import androidx.annotation.RawRes
+import com.ninovanhooff.negenwnegenw.R
 import com.ninovanhooff.negenwnegenw.services.TimeSlot
 import kotlin.math.roundToInt
 
@@ -10,9 +12,21 @@ data class TodayModel(
     val tempUnit: String,
     val feelsLike: String,
     val tempMin: String, val tempMax: String,
-    val weatherDescription: String
+    val weatherDescription: String,
+    @RawRes val animationRawRes: Int
 ){
     companion object {
+        /** Maps OpenWeather Main labels to Lottie animations.
+         * For a more precise (and possibly more stable) classification, id codes could be used
+         * https://openweathermap.org/weather-conditions#Weather-Condition-Codes
+         */
+        private val lottieMap = mapOf(
+            "Clear" to R.raw.lottie_sunny,
+            "Clouds" to R.raw.lottie_windy,
+            "Thunderstorm" to R.raw.lottie_thunderstorm,
+            "Rain" to R.raw.lottie_partly_shower
+        )
+
         fun fromTimeSlot(timeSlot: TimeSlot): TodayModel {
             return TodayModel(
                 timeSlot.main.temp.convertTemp(),
@@ -20,7 +34,8 @@ data class TodayModel(
                 timeSlot.main.feels_like.convertTemp(),
                 timeSlot.main.temp_min.convertTemp(),
                 timeSlot.main.temp_max.convertTemp(),
-                timeSlot.weather[0].description
+                timeSlot.weather[0].description,
+                lottieMap[timeSlot.weather[0].main] ?: R.raw.lottie_partly_cloudy
             )
         }
 
