72e3bb3958c3151049a0af796fa5bd47e760a4af
Nino van Hooff <ninovanhooff@gmail.com>
2020-06-14 14:21:02 +0200

    Fix network caching

--- app/src/main/java/com/ninovanhooff/negenwnegenw/services/WeatherService.kt
+++ app/src/main/java/com/ninovanhooff/negenwnegenw/services/WeatherService.kt
@@ -6,13 +6,13 @@ import com.ninovanhooff.negenwnegenw.services.dto.FindResponse
 import com.ninovanhooff.negenwnegenw.services.dto.FiveDayForecastResponse
 import okhttp3.Cache
 import okhttp3.CacheControl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import timber.log.Timber
 import java.io.File
 import java.util.concurrent.TimeUnit
 
@@ -43,14 +43,14 @@ interface WeatherService {
             val myCache = Cache(cacheDir, CACHE_SIZE)
 
             val defaultCacheControl = CacheControl.Builder()
-                .maxAge(15, TimeUnit.MINUTES) // 15 minutes cache
+                .maxAge(30, TimeUnit.MINUTES)
                 .build()
                 .toString()
 
 
             val httpClient = OkHttpClient.Builder()
             httpClient.cache(myCache)
-            httpClient.addInterceptor { chain ->
+            httpClient.addNetworkInterceptor { chain ->
                 val originalRequest = chain.request()
 
                 // Request customization: add request headers
@@ -60,20 +60,21 @@ interface WeatherService {
 
                 val request = requestBuilder.build()
 
-                Timber.d("Requesting ${request.url()}")
-                val originalResponse = chain.proceed(request)
+                chain.proceed(request)
+            }
 
+            /** Dangerous interceptor that rewrites the server's cache-control header. */
+            val rewriteCacheControlInterceptor: Interceptor = object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+                    val originalResponse = chain.proceed(chain.request())
 
-                // Use a default caching policy if not provided
-                if (originalResponse.header("Cache-Control").isNullOrBlank()) {
-                    originalResponse.newBuilder()
+                    return originalResponse.newBuilder()
                         .header("Cache-Control", defaultCacheControl)
                         .build()
-                } else {
-                    originalResponse
                 }
-
             }
+
+            httpClient.addNetworkInterceptor(rewriteCacheControlInterceptor)
 
             val okHttp = httpClient.build()
 
